f0271cc106dbf278b1934669a442663af8eeacc5
valhallaIamcoming <anjan@friday>
2019-02-27 02:12:45 +0530

    Cahnged the UI; TODO: Refactor the code; Add imageprocessing tools

--- draw.js
+++ draw.js
@@ -1,151 +1,161 @@
-    var canvas, ctx, flag = false,
-        prevX = 0,
-        currX = 0,
-        prevY = 0,
-        currY = 0,
-        dot_flag = false;
+var canvas, ctx, flag = false,
+prevX = 0,
+currX = 0,
+prevY = 0,
+currY = 0,
+dot_flag = false;
 
-    var x = "red",
-        y = 2;
-    
-    function init() 
-    {
-        canvas = document.getElementById("Q3");
-        ctx = canvas.getContext("2d");
-        w = canvas.width;
-        h = canvas.height;
+var x = "red",
+y = 2;
 
-        // ctx.fillStyle = "#FF0000";
-        // ctx.fillRect(0, 0, w, h);
+function init() 
+{
+    canvas = document.getElementById("Q3");
+    ctx = canvas.getContext("2d");
+    w = canvas.width;
+    h = canvas.height;
+    canvas.addEventListener
+                ("mousemove", 
+                    function (e) 
+                    {
+                        findxy('move', e) ; 
+                    }, 
+                    false
+                );
 
-        // alert(w);
-        // alert(h);
+    canvas.addEventListener
+                ("mousedown", 
+                    function (e) 
+                    {
+                        findxy('down', e) 
+                    }, 
+                    false
+                );
 
-        canvas.addEventListener("mousemove", 
-            function (e) 
+    canvas.addEventListener
+                ("mouseup", 
+                    function (e) 
+                    {
+                        findxy('up', e)
+                    }, 
+                    false
+                );
+
+    canvas.addEventListener
+                ("mouseout", 
+                    function (e) 
+                    {
+                        findxy('out', e)
+                    }, 
+                    false
+                );
+}
+
+
+
+
+        
+        function color(obj,size) 
+        {
+            //alert(obj.id);
+            if(obj.id == "black")
             {
-                // document.getElementById('test').innerHTML = "hi";
-            findxy('move', e) ; 
-            // console.log("in listener"); alert("in listener");
-            }, false);
+                ctx.globalCompositeOperation = "source-over";
+                // alert("inb");
+                x = "rgba(255,0,0)";
+                y = size;
+            }
+            else
+            {
+                ctx.globalCompositeOperation = "destination-out"
+                // x = "rgba(255,0 ,0,0)";
+                y = size;
+            }
+            // switch (obj.id) 
+            // {
 
-        // alert(w);
-        canvas.addEventListener("mousedown", function (e) {
-            findxy('down', e) 
-        }, false);
-        canvas.addEventListener("mouseup", function (e) {
-            findxy('up', e)
-        }, false);
-        canvas.addEventListener("mouseout", function (e) {
-            findxy('out', e)
-        }, false);
-    }
+            //     alert("in objid");
+            //     case "black":
+            //         
+            //         break;
+            //     case "white":
+            //         
+            //         break;
+            // }
 
+            // y = 12;
+            // if (x == "white") y = 14;
+            // else y = 2;
 
-
-
-    
-    function color(obj,size) 
-    {
-        //alert(obj.id);
-        if(obj.id == "black")
+        }
+        
+        function draw() 
         {
-            ctx.globalCompositeOperation = "source-over";
-            // alert("inb");
-            x = "rgba(255,0,0)";
-            y = size;
+
+            ctx.beginPath();
+            ctx.moveTo(prevX, prevY);
+            ctx.lineTo(currX, currY);
+            ctx.strokeStyle = x;
+
+
+
+            ctx.lineWidth = y;
+            ctx.stroke();
+            ctx.closePath();
         }
-        else
-        {
-            ctx.globalCompositeOperation = "destination-out"
-            // x = "rgba(255,0 ,0,0)";
-            y = size;
-        }
-        // switch (obj.id) 
-        // {
+        
+        function erase() {
+            // var m = confirm("Want to clear");
+            {
 
-        //     alert("in objid");
-        //     case "black":
-        //         
-        //         break;
-        //     case "white":
-        //         
-        //         break;
-        // }
+                // ctx.globalCompositeOperation = "destination-out";
+                // ctx.strokeStyle = "rgba(255,0,0,100)";
 
-        // y = 12;
-        // if (x == "white") y = 14;
-        // else y = 2;
-    
-    }
-    
-    function draw() 
-    {
-            
-        ctx.beginPath();
-        ctx.moveTo(prevX, prevY);
-        ctx.lineTo(currX, currY);
-        ctx.strokeStyle = x;
-      
-
-
-        ctx.lineWidth = y;
-        ctx.stroke();
-        ctx.closePath();
-    }
-    
-    function erase() {
-        // var m = confirm("Want to clear");
-         {
-
-            // ctx.globalCompositeOperation = "destination-out";
-            // ctx.strokeStyle = "rgba(255,0,0,100)";
-
-            ctx.clearRect(0, 0, w, h);
-            document.getElementById("Q3").style.display = "none";
-        }
-    }
-    
-    function save() {
-        document.getElementById("Q3").style.border = "2px solid";
-        var dataURL = canvas.toDataURL();
-        document.getElementById("Q3").src = dataURL;
-        document.getElementById("Q3").style.display = "inline";
-    }
-    
-    function findxy(res, e) 
-    {
-        // alert("in fxy");
-        // console.log("in fxy");
-        if (res == 'down') {
-            prevX = currX;
-            prevY = currY;
-            currX = e.layerX;
-            currY = e.layerY ;
-    
-            flag = true;
-            dot_flag = true;
-            if (dot_flag) {
-                ctx.beginPath();
-                ctx.fillStyle = x;
-                ctx.fillRect(currX, currY, 2, 2);
-                ctx.closePath();
-                dot_flag = false;
+                ctx.clearRect(0, 0, w, h);
+                document.getElementById("Q3").style.display = "none";
             }
         }
-        if (res == 'up' || res == "out") {
-            flag = false;
+        
+        function save() {
+            document.getElementById("Q3").style.border = "2px solid";
+            var dataURL = canvas.toDataURL();
+            document.getElementById("Q3").src = dataURL;
+            document.getElementById("Q3").style.display = "inline";
         }
-        if (res == 'move') {
-            if (flag) {
+        
+        function findxy(res, e) 
+        {
+            // alert("in fxy");
+            // console.log("in fxy");
+            if (res == 'down') {
                 prevX = currX;
                 prevY = currY;
                 currX = e.layerX;
-                currY = e.layerY;
-                draw();
+                currY = e.layerY ;
+
+                flag = true;
+                dot_flag = true;
+                if (dot_flag) {
+                    ctx.beginPath();
+                    ctx.fillStyle = x;
+                    ctx.fillRect(currX, currY, 2, 2);
+                    ctx.closePath();
+                    dot_flag = false;
+                }
+            }
+            if (res == 'up' || res == "out") {
+                flag = false;
+            }
+            if (res == 'move') {
+                if (flag) {
+                    prevX = currX;
+                    prevY = currY;
+                    currX = e.layerX;
+                    currY = e.layerY;
+                    draw();
+                }
             }
         }
-    }
 
-init();
-// window.onload = init();
+        init();
+    // window.onload = init();
